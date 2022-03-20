07f4025738b25f6a00262646feba198e9d16a42f
Lem Canady <lem.canady@gmail.com>
2022-03-19 20:22:27 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import bg from "./assets/bg.png";
+import logo from "./assets/logo.png";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${bg});
+  background-size: cover;
+  background-position: center;
+`;
+
+const Overlay = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.9);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  text-transform: uppercase;
+  letter-spacing: 0.7rem;
+  margin-top: 24px;
+`;
+
+const Button = styled.button`
+  margin-top: 24px;
+  height: 72px;
+  min-width: 300px;
+  font-size: 1.3em;
+  border-radius: 50px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Overlay>
+        <img src={logo} alt="logo" />
+        <Title>Vortex Pets Pass</Title>
+        <Button>Mint</Button>
+      </Overlay>
+    </Wrapper>
   );
 }
 
